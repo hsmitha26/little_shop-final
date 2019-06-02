762b2dede8438e5bb1eeca45b0bc1a1a77f83f72
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-06-02 16:18:19 -0600

    adds feature test to see if merchant has sufficient inventory for each item

--- spec/features/merchants/dashboard_to_do_spec.rb
+++ spec/features/merchants/dashboard_to_do_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Merchant dashboard: ' do
     @other_merchant = create(:merchant)
     @i4 = create(:item, user: @other_merchant)
 
-    @o1, @o2 = create_list(:order, 2)
+    @o1, @o2, @o5 = create_list(:order, 3)
     @o3 = create(:shipped_order)
     @o4 = create(:cancelled_order)
 
@@ -20,6 +20,8 @@ RSpec.describe 'Merchant dashboard: ' do
 
     @oi3 = create(:order_item, order: @o2, item: @i2, quantity: 4, price: 2)
     @oi6 = create(:order_item, order: @o2, item: @i4, quantity: 2, price: 2)
+
+    @oi7 = create(:order_item, order: @o3, item: @i3, quantity: 9, price: 1)
 
     #shipped order
     @oi4 = create(:order_item, order: @o3, item: @i1, quantity: 4, price: 2)
@@ -62,8 +64,23 @@ RSpec.describe 'Merchant dashboard: ' do
         expect(page).to have_content("Yes")
       end
 
-      expect(page).to_not have_content(@o3.id)
-      expect(page).to_not have_content(@o4.id)
+      expect(page).to_not have_content(@o3)
+      expect(page).to_not have_content(@o4)
+    end
+
+    it "should check all orders and let merchant know if they sufficient inventory for each item" do
+      # binding.pry
+      within "#items-to-do-#{@i1.id}" do
+        expect(page).to have_content("Yes")
+      end
+
+      within "#items-to-do-#{@i2.id}" do
+        expect(page).to have_content("Yes")
+      end
+
+      within "#items-to-do-#{@i3.id}" do
+        expect(page).to have_content("No")
+      end
     end
   end
 end
